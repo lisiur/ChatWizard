207cccc2a545d765aa31dfe99a2bc378839344dd
lisiur <lisiurday@gmail.com>
2023-03-13 22:28:32 +0800

    feat: support auto update
    fix #7

---
+++ gui/web/src/hooks/version.ts
@@ -0,0 +1,59 @@
+import { app } from "@tauri-apps/api";
+import {
+  checkUpdate,
+  installUpdate,
+  UpdateManifest,
+  UpdateStatus,
+  UpdateStatusResult,
+} from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
+import { emit, Event, listen } from "@tauri-apps/api/event";
+import { ref } from "vue";
+
+export function useVersion() {
+  const version = ref("");
+  app.getVersion().then((v) => {
+    version.value = v;
+  });
+
+  const hasNewVersion = ref(false);
+  const newVersion = ref<UpdateManifest>();
+  checkUpdate().then(({ shouldUpdate, manifest }) => {
+    hasNewVersion.value = shouldUpdate;
+    newVersion.value = manifest;
+  });
+
+  function installNewVersion() {
+    emit("tauri://update-install");
+    return new Promise(async (resolve, reject) => {
+      const unListen = await listen(
+        "tauri://update-status",
+        (res: Event<UpdateStatusResult>) => {
+          switch (res.payload.status) {
+            case "PENDING": {
+              return;
+            }
+            case "DONE": {
+              resolve(true);
+              unListen();
+              return;
+            }
+            case "ERROR": {
+              reject(res.payload.error);
+              unListen();
+              return;
+            }
+          }
+        }
+      );
+    });
+  }
+
+  return {
+    version,
+    hasNewVersion,
+    newVersion,
+    installNewVersion,
+    relaunch,
+  };
+}
