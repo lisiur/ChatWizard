88969b4d5dc49c9e67331b0182b8569e8247ba5b
lisiur <lisiurday@gmail.com>
2023-04-04 10:10:47 +0800

    wip: support web server

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,76 +1,62 @@
-mod chat;
-mod client;
-mod completion;
 mod error;
-mod image;
-mod models;
 mod result;
-mod types;
 
-pub use chat::{Chat, ChatParams, Message, Role, StreamContent};
-use client::{Client, ClientOpts};
-use error::ApiErrorResponse;
+use std::net::SocketAddr;
+
+use axum::http::Method;
+use axum::response::IntoResponse;
+use axum::{routing::get, Router};
+
 pub use error::Error;
 pub use result::Result;
 
-pub struct OpenAIApi {
-    client: Client,
-    host: String,
+use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
+use tower_http::cors::{Any, CorsLayer};
+
+pub async fn app(port: u16) {
+    let app = Router::new().route("/ws", get(ws_handler)).layer(
+        CorsLayer::new().allow_origin(Any).allow_methods(vec![
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::PATCH,
+            Method::DELETE,
+        ]),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-impl OpenAIApi {
-    pub fn new(api_key: Option<&str>) -> Self {
-        Self {
-            client: Self::create_client(api_key, None),
-            host: "https://api.openai.com".to_string(),
-        }
-    }
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
 
-    fn create_client(api_key: Option<&str>, proxy: Option<&str>) -> Client {
-        let mut headers = reqwest::header::HeaderMap::new();
-
-        if let Some(api_key) = api_key {
-            headers.insert(
-                reqwest::header::AUTHORIZATION,
-                format!("Bearer {api_key}").parse().unwrap(),
-            );
-        }
-
-        let proxy = proxy.map(|item| reqwest::Proxy::all(item).unwrap());
-
-        Client::new(ClientOpts {
-            headers: Some(headers),
-            proxy,
-        })
-    }
-
-    pub fn set_host(&mut self, host: &str) {
-        self.host = host.to_string();
-    }
-
-    pub fn set_proxy(&mut self, proxy: &str) {
-        if proxy.is_empty() {
-            self.client.clear_proxy();
-        } else {
-            self.client.set_proxy(reqwest::Proxy::all(proxy).unwrap());
-        }
-    }
-
-    pub async fn clear_proxy(&mut self) {
-        self.client.clear_proxy();
-    }
-
-    pub async fn check_api_key(&self, api_key: &str) -> Result<()> {
-        let client = Self::create_client(Some(api_key), None);
-        let response = client
-            .get(&format!("{}/v1/models", self.host), None)
-            .await?;
-
-        if response.status().is_success() {
-            Ok(())
-        } else {
-            let error: ApiErrorResponse = response.json().await?;
-            Err(error.into())
+async fn handle_socket(mut socket: WebSocket) {
+    loop {
+        if let Some(msg) = socket.recv().await {
+            if let Ok(msg) = msg {
+                match msg {
+                    Message::Text(t) => {
+                        if socket
+                            .send(Message::Text(format!("Echo from backend: {}", t)))
+                            .await
+                            .is_err()
+                        {
+                            return;
+                        }
+                    }
+                    Message::Close(_) => {
+                        return;
+                    }
+                    _ => {}
+                }
+            } else {
+                return;
+            }
         }
     }
 }
