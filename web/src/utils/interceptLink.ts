f7e1d0716f7104ef286ca84182294f088fa49eaa
lisiur <lisiurday@gmail.com>
2023-04-10 17:15:41 +0800

    fix: fix open link failed on web

--- web/src/utils/interceptLink.ts
+++ web/src/utils/interceptLink.ts
@@ -1,11 +1,16 @@
 import { invoke } from "./api";
+import { isTauri } from "./env";
 
 export function interceptLink(dom: HTMLElement) {
   dom.querySelectorAll("a").forEach(function (a) {
     if (a.href.startsWith("http")) {
       a.addEventListener("click", function (e) {
         e.preventDefault();
-        invoke("open", { url: a.href });
+        if (isTauri) {
+          invoke("open", { url: a.href });
+        } else {
+          window.open(a.href, "_blank");
+        }
       });
     }
   });
